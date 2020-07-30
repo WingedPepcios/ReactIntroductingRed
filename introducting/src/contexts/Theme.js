22408398b8b81440ad7b8603b0e711c3293ef35a
WingedPepcios <piskapiotr@winged.pl>
2020-07-30 21:01:33 +0200

    Context 2

--- introducting/src/contexts/Theme.js
+++ introducting/src/contexts/Theme.js
@@ -9,9 +9,22 @@ export const defaultValues = {
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(defaultValues);
-  
+
+  const changeTheme = (type) => {
+    switch (type) {
+      case 'LIGHT':
+        setTheme({ backgroundColor: '#999', textColor: '#333', });
+        break;
+      case 'DARK':
+        setTheme({ backgroundColor: '#000', textColor: '#fff', });
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <ThemeContext.Provider value={{ ...theme, setTheme }}>
+    <ThemeContext.Provider value={{ ...theme, changeTheme }}>
       {children}
     </ThemeContext.Provider>
   )
--- introducting/src/molecules/Logo.jsx
+++ introducting/src/molecules/Logo.jsx
@@ -3,15 +3,15 @@ import { ThemeContext } from '../contexts/Theme';
 import Button from '../atoms/Button';
 
 const Logo = () => {
-  const { backgroundColor, setTheme } = useContext(ThemeContext);
+  const { backgroundColor, changeTheme } = useContext(ThemeContext);
 
   return (
     <div>
       <a href="/">
         <div style={{ width: 100, height: 100, backgroundColor }}></div>
       </a>
-      <Button type="button" onClick={() => setTheme({ backgroundColor: '#000' })}>Ciemny styl</Button>
-      <Button type="button" onClick={() => setTheme({ backgroundColor: '#999' })}>Jasny styl</Button>
+      <Button type="button" onClick={() => changeTheme("DARK")}>Ciemny styl</Button>
+      <Button type="button" onClick={() => changeTheme("LIGHT")}>Jasny styl</Button>
     </div>
   );
 };
